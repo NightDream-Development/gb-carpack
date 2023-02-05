6c9bdf31f0aab290c8ae863b924ba110e19479f2
GoodbananaHUN <73757004+GoodbananaHUN@users.noreply.github.com>
2023-02-05 15:48:44 +0100

    328nii

---
+++ 328nii/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version  'cerulean'
+games       { 'gta5' }
+author      'Akkariin'
+version     '1.0.0'
+
+files {
+	'carcols.meta',
+	'carvariations.meta',
+	'dlctext.meta',
+	'handling.meta',
+	'vehicles.meta',
+	'*.meta'
+}
+
+data_file 'DLCTEXT_FILE' 'dlctext.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
